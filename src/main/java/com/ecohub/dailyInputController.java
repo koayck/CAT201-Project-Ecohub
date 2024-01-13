dc1ea327edc50035194ab4121bc80407665efa57
Johnas <johnaslee97@gmail.com>
2024-01-13 22:57:08 +0800

    Created first version of daily input

---
+++ src/main/java/com/ecohub/dailyInputController.java
@@ -0,0 +1,93 @@
+package com.ecohub;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
+public class dailyInputController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private MenuItem AC;
+
+    @FXML
+    private MenuItem Bathing;
+
+    @FXML
+    private MenuItem Car;
+
+    @FXML
+    private MenuItem Drinking;
+
+    @FXML
+    private MenuItem Electronic;
+
+    @FXML
+    private MenuItem Plastic;
+
+    @FXML
+    private MenuItem Refrigerator;
+
+    @FXML
+    private MenuItem Walking;
+
+    @FXML
+    private MenuItem Washing;
+
+    @FXML
+    void AC(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Bathing(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Car(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Drinking(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Electronic(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Plastic(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Refrigerator(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Walking(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Washing(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
