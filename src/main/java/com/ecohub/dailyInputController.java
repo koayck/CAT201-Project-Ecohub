c99dae701adc4c464aea92c64bc80814b8cdd14e
Johnas <johnaslee97@gmail.com>
2024-01-14 01:26:42 +0800

    Your selection will now appear next to the menu list! Message will show up once the submit button is pressed

--- src/main/java/com/ecohub/dailyInputController.java
+++ src/main/java/com/ecohub/dailyInputController.java
@@ -3,6 +3,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 
 public class dailyInputController {
@@ -11,7 +12,22 @@ public class dailyInputController {
     private ResourceBundle resources;
 
     @FXML
+    private Label submitLabel;
+
+    @FXML
     private URL location;
+
+    @FXML
+    private Label distanceLabel;
+
+    @FXML
+    private Label electricLabel;
+
+    @FXML
+    private Label wasteLabel;
+
+    @FXML
+    private Label waterLabel;
 
     @FXML
     private MenuItem AC;
@@ -42,47 +58,53 @@ public class dailyInputController {
 
     @FXML
     void AC(ActionEvent event) {
-
+        electricLabel.setText("AC"); 
     }
 
     @FXML
     void Bathing(ActionEvent event) {
-
+        waterLabel.setText("Bathing");   
     }
 
     @FXML
     void Car(ActionEvent event) {
-
+        distanceLabel.setText("Car");
     }
 
     @FXML
     void Drinking(ActionEvent event) {
-
+        waterLabel.setText("Drinking");
     }
 
     @FXML
     void Electronic(ActionEvent event) {
-
+        wasteLabel.setText("Electronic");
     }
 
     @FXML
     void Plastic(ActionEvent event) {
-
+        wasteLabel.setText("Plastic"); 
     }
 
     @FXML
     void Refrigerator(ActionEvent event) {
-
+        electricLabel.setText("Refrigerator"); 
     }
 
     @FXML
     void Walking(ActionEvent event) {
-
+        distanceLabel.setText("Walking");
     }
+
 
     @FXML
     void Washing(ActionEvent event) {
-
+        wasteLabel.setText("Washing");
+    }
+    
+    @FXML
+    private void handleSubmit(ActionEvent event) {
+        submitLabel.setText("Submitted Sucessfully!");
     }
 
     @FXML
