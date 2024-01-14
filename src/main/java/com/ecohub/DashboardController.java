3227b836333b85b9682b38f1c5a4b8d1347adbfd
Jisi-A <7216lyc@gmail.com>
2024-01-14 22:25:10 +0800

    ✨Complete actions for all buttons

--- src/main/java/com/ecohub/DashboardController.java
+++ src/main/java/com/ecohub/DashboardController.java
@@ -2,12 +2,16 @@ package com.ecohub;
 
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
@@ -26,49 +30,144 @@ public class DashboardController {
     @FXML
     private StackPane rightPane;
 
-    @FXML
-    void showBreak(MouseEvent event) {
-
-    }
-
+    
     @FXML
     void showCarbon(MouseEvent event) {
         styleBox(0);
         CategoryAxis xAxis = new CategoryAxis();
         xAxis.setLabel("Date");
-
+        
         NumberAxis yAxis = new NumberAxis();
         yAxis.setLabel("Carbon Footprint");
-
+        
         LineChart<String, Number> carbonFoorprint = new LineChart<String, Number>(xAxis, yAxis);
         carbonFoorprint.setTitle("Daily Carbon Footprint");
-
+        
         XYChart.Series<String, Number> data = new XYChart.Series<String, Number>();
         data.setName("Total Carbon Footprint");
-
+        
         data.getData().add(new XYChart.Data<String, Number>("14/1", 200));
         data.getData().add(new XYChart.Data<String, Number>("15/1", 400));
         data.getData().add(new XYChart.Data<String, Number>("16/1", 100));
-
+        
         carbonFoorprint.getData().add(data);
         carbonFoorprint.setLegendVisible(false);
-
+        
         chartView.getChildren().clear();
         chartView.getChildren().add(carbonFoorprint);
     }
 
     @FXML
-    void showElectric(MouseEvent event) {
+    void showBreak(MouseEvent event) {
+        styleBox(1);
 
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+            new PieChart.Data("Fuel", 3000),
+            new PieChart.Data("Electricity", 1500),
+            new PieChart.Data("Water", 1020),
+            new PieChart.Data("Waste", 2030)
+        );
+
+        PieChart pieChart = new PieChart(pieChartData);
+        pieChart.setTitle("Carbon Footprint Break Down");
+        pieChart.setClockwise(true);
+        pieChart.setLabelLineLength(50);
+        pieChart.setLabelsVisible(true);
+        pieChart.setStartAngle(180);
+
+        chartView.getChildren().clear();
+        chartView.getChildren().add(pieChart);
     }
 
     @FXML
     void showTravel(MouseEvent event) {
+        styleBox(2);
 
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Date");
+        
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Traveled Distance");
+        
+        LineChart<String, Number> distanceTravel = new LineChart<String, Number>(xAxis, yAxis);
+        distanceTravel.setTitle("Daily Traveled Distance");
+        
+        XYChart.Series<String, Number> data = new XYChart.Series<String, Number>();
+        data.setName("Total Traveled Distance");
+        
+        data.getData().add(new XYChart.Data<String, Number>("14/1", 1200));
+        data.getData().add(new XYChart.Data<String, Number>("15/1", 403));
+        data.getData().add(new XYChart.Data<String, Number>("16/1", 830));
+        
+        distanceTravel.getData().add(data);
+        distanceTravel.setLegendVisible(false);
+        
+        chartView.getChildren().clear();
+        chartView.getChildren().add(distanceTravel);
     }
+
+    @FXML
+    void showElectric(MouseEvent event) {
+        styleBox(3);
+
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Date");
+        
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Electric Usage");
+        
+        BarChart<String, Number> electricUsage = new BarChart<String, Number>(xAxis, yAxis);
+        electricUsage.setTitle("Daily Electric Usage");
+        
+        XYChart.Series<String, Number> data = new XYChart.Series<String, Number>();
+        data.setName("Total Carbon Footprint");
+        
+        data.getData().add(new XYChart.Data<String, Number>("14/1", 200));
+        data.getData().add(new XYChart.Data<String, Number>("15/1", 400));
+        data.getData().add(new XYChart.Data<String, Number>("16/1", 100));
+        
+        electricUsage.getData().add(data);
+        electricUsage.setLegendVisible(false);
+        
+        chartView.getChildren().clear();
+        chartView.getChildren().add(electricUsage);
+    }
+
 
     private void styleBox(int index) {
         // This function change the style+color of the menu (Menu Item Selected)
+        show1.setStyle("-fx-background-color: white;");
+        for (javafx.scene.Node node : show1.getChildren()) {
+            if (node instanceof Label) {
+                // Modify the style of the label here
+                Label label = (Label) node;
+                label.setStyle("-fx-text-fill: black;");
+            }
+        }
+        show2.setStyle("-fx-background-color: white;");
+        for (javafx.scene.Node node : show2.getChildren()) {
+            if (node instanceof Label) {
+                // Modify the style of the label here
+                Label label = (Label) node;
+                label.setStyle("-fx-text-fill: black;");
+            }
+        }
+        show3.setStyle("-fx-background-color: white;");
+        for (javafx.scene.Node node : show3.getChildren()) {
+            if (node instanceof Label) {
+                // Modify the style of the label here
+                Label label = (Label) node;
+                label.setStyle("-fx-text-fill: black;");
+            }
+        }
+        show4.setStyle("-fx-background-color: white;");
+        for (javafx.scene.Node node : show4.getChildren()) {
+            if (node instanceof Label) {
+                // Modify the style of the label here
+                Label label = (Label) node;
+                label.setStyle("-fx-text-fill: black;");
+            }
+        }
 
         switch (index) {
             case 0:
@@ -82,13 +181,34 @@ public class DashboardController {
                 }
                 break;
             case 1:
-                show2.setStyle("-fx-background-color: #f2f2f2;-fx-border-color: #0078D7;-fx-border-width: 0px 0px 0px 3px;-fx-border-style: solid;");
+                show2.setStyle("-fx-background-color: #10c474;");
+                for (javafx.scene.Node node : show2.getChildren()) {
+                    if (node instanceof Label) {
+                        // Modify the style of the label here
+                        Label label = (Label) node;
+                        label.setStyle("-fx-text-fill: white;");
+                    }
+                }
                 break;
             case 2:
-                show3.setStyle("-fx-background-color: #f2f2f2;-fx-border-color: #0078D7;-fx-border-width: 0px 0px 0px 3px;-fx-border-style: solid;");
+                show3.setStyle("-fx-background-color: #10c474;");
+                for (javafx.scene.Node node : show3.getChildren()) {
+                    if (node instanceof Label) {
+                        // Modify the style of the label here
+                        Label label = (Label) node;
+                        label.setStyle("-fx-text-fill: white;");
+                    }
+                }
                 break;
             case 3:
-                show4.setStyle("-fx-background-color: #f2f2f2;-fx-border-color: #0078D7;-fx-border-width: 0px 0px 0px 3px;-fx-border-style: solid;");
+                show4.setStyle("-fx-background-color: #10c474;");
+                for (javafx.scene.Node node : show4.getChildren()) {
+                    if (node instanceof Label) {
+                        // Modify the style of the label here
+                        Label label = (Label) node;
+                        label.setStyle("-fx-text-fill: white;");
+                    }
+                }
                 break;
         }
     }
