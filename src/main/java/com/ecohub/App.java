76d760e77dc2e45220a51fab4e0e76ce0fe1924e
dejie <dejieang168@gmail.com>
2024-01-16 11:45:31 +0800

    🚧 design to be improved

--- src/main/java/com/ecohub/App.java
+++ src/main/java/com/ecohub/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("home"));
+        scene = new Scene(loadFXML("dailyInput"));
         stage.setScene(scene);
         stage.show();
     }
@@ -27,6 +27,7 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
+        System.out.println(fxml);
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
