8eef6236ec11f2669041caf4bed38d8bb35d87b7
dejie <dejieang168@gmail.com>
2024-01-18 23:25:00 +0800

    🎨 move calculatation of total and percentage to RecordDAO.java

--- src/main/java/com/ecohub/dao/RecordDAO.java
+++ src/main/java/com/ecohub/dao/RecordDAO.java
@@ -2,6 +2,7 @@ package com.ecohub.dao;
 
 import com.ecohub.models.Record;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,29 +12,20 @@ import java.util.List;
 
 public class RecordDAO {
 
-  private static final String INSERT_QUERY =
-    "INSERT INTO ECOHUB.RECORD (R_CATEGORY, R_TITLE, R_VALUE, U_ID) VALUES (?,?,?,?)";
-  private static final String DELETE_QUERY =
-    "DELETE FROM ECOHUB.RECORD WHERE R_ID = ?";
-  private static final String SELECT_ALL_QUERY =
-    "SELECT * FROM ECOHUB.RECORD WHERE U_ID = ?";
-  private static final String UPDATE_QUERY =
-    "UPDATE ECOHUB.RECORD SET R_CATEGORY = ?, R_TITLE = ?, R_VALUE = ? WHERE R_ID = ?";
+  private static final String INSERT_QUERY = "INSERT INTO ECOHUB.RECORD (R_CATEGORY, R_TITLE, R_VALUE, U_ID) VALUES (?,?,?,?)";
+  private static final String DELETE_QUERY = "DELETE FROM ECOHUB.RECORD WHERE R_ID = ?";
+  private static final String SELECT_ALL_QUERY = "SELECT * FROM ECOHUB.RECORD WHERE U_ID = ?";
+  private static final String UPDATE_QUERY = "UPDATE ECOHUB.RECORD SET R_CATEGORY = ?, R_TITLE = ?, R_VALUE = ? WHERE R_ID = ?";
 
-
-  // function for adding record based on this query private static final String INSERT_QUERY =
+  // function for adding record based on this query private static final String
+  // INSERT_QUERY =
   // "INSERT INTO ECOHUB.RECORD (R.CATEGORY, R.TITLE, R.VALUE) VALUES (?,?,?)";
-  public void addRecord(String category, String title, String value, int uid)
-    throws SQLException {
+  public void addRecord(String category, String title, String value, int uid) throws SQLException {
     // Convert the value string to a BigDecimal
     BigDecimal bdValue = new BigDecimal(value);
 
-    try (
-      Connection connection = DBUtil.getConnection();
-      PreparedStatement preparedStatement = connection.prepareStatement(
-        INSERT_QUERY
-      )
-    ) {
+    try (Connection connection = DBUtil.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY)) {
       preparedStatement.setString(1, category);
       preparedStatement.setString(2, title);
       preparedStatement.setBigDecimal(3, bdValue);
@@ -45,15 +37,42 @@ public class RecordDAO {
     }
   }
 
-  // function for deleting record based on this query private static final String DELETE_QUERY =
+  public BigDecimal calculateValue(String title, BigDecimal input) {
+    BigDecimal value;
+
+    if ("Car".equals(title)) {
+      value = input.multiply(BigDecimal.valueOf(0.07)).multiply(BigDecimal.valueOf(2.349));
+    } else if ("Walking".equals(title)) {
+      value = input.multiply(BigDecimal.valueOf(0.039));
+    } else if (("AC".equals(title) || "Refrigerator".equals(title))) {
+      value = input.multiply(BigDecimal.valueOf(0.39));
+    } else if ("Plastic".equals(title)) {
+      value = input.multiply(BigDecimal.valueOf(6));
+    } else if ("Electronic".equals(title)) {
+      value = input.multiply(BigDecimal.valueOf(2));
+    } else if (("Drinking".equals(title) || "Bathing".equals(title) || "Washing".equals(title))) {
+      value = input.multiply(BigDecimal.valueOf(0.298));
+    } else {
+      value = input.multiply(BigDecimal.valueOf(0));
+    }
+
+    value = value.setScale(8, RoundingMode.HALF_UP);
+    return value;
+  }
+
+  // calculate percentage for each activity
+  public BigDecimal calculatePercentage(String activity, BigDecimal input, BigDecimal total) {
+    BigDecimal value = calculateValue(activity, input);
+    BigDecimal percentage = value.divide(total, 2, RoundingMode.HALF_UP).multiply(BigDecimal.valueOf(100));
+    return percentage;
+  }
+
+  // function for deleting record based on this query private static final String
+  // DELETE_QUERY =
   // "DELETE FROM ECOHUB.RECORD WHERE R_ID = ?";
   public void deleteRecord(int id) throws SQLException {
-    try (
-      Connection connection = DBUtil.getConnection();
-      PreparedStatement preparedStatement = connection.prepareStatement(
-        DELETE_QUERY
-      )
-    ) {
+    try (Connection connection = DBUtil.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(DELETE_QUERY)) {
       preparedStatement.setInt(1, id);
       preparedStatement.executeUpdate();
     } catch (SQLException e) {
@@ -61,27 +80,21 @@ public class RecordDAO {
     }
   }
 
-  // function for selecting all record based on this query private static final String SELECT_QUERY =
+  // function for selecting all record based on this query private static final
+  // String SELECT_QUERY =
   // "SELECT * FROM ECOHUB.RECORD WHERE U_ID = ?";
-public List<Record> getAllRecords(int id) throws SQLException {
+  public List<Record> getAllRecords(int id) throws SQLException {
     List<Record> recordList = new ArrayList<>();
-    try (
-      Connection connection = DBUtil.getConnection();
-      PreparedStatement preparedStatement = connection.prepareStatement(
-        SELECT_ALL_QUERY
-      )
-    ) {
+    try (Connection connection = DBUtil.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_QUERY)) {
       preparedStatement.setInt(1, id);
-      
+
       ResultSet resultSet = preparedStatement.executeQuery();
       while (resultSet.next()) {
         // add each record to the list
 
-        Record record = new Record(
-          resultSet.getString("R_CATEGORY"),
-          resultSet.getString("R_TITLE"),
-          resultSet.getBigDecimal("R_VALUE")
-        );
+        Record record = new Record(resultSet.getString("R_CATEGORY"), resultSet.getString("R_TITLE"),
+            resultSet.getBigDecimal("R_VALUE"));
         recordList.add(record);
       }
     } catch (SQLException e) {
@@ -90,19 +103,16 @@ public List<Record> getAllRecords(int id) throws SQLException {
     return recordList;
   }
 
-  // function for updating record based on this query private static final String UPDATE_QUERY =
-  // "UPDATE ECOHUB.RECORD SET R.CATEGORY = ?, R.TITLE = ?, R.VALUE = ? WHERE R_ID = ?";
-  public void updateRecord(String category, String title, String value, int id)
-    throws SQLException {
+  // function for updating record based on this query private static final String
+  // UPDATE_QUERY =
+  // "UPDATE ECOHUB.RECORD SET R.CATEGORY = ?, R.TITLE = ?, R.VALUE = ? WHERE R_ID
+  // = ?";
+  public void updateRecord(String category, String title, String value, int id) throws SQLException {
     // Convert the value string to a BigDecimal
     BigDecimal bdValue = new BigDecimal(value);
 
-    try (
-      Connection connection = DBUtil.getConnection();
-      PreparedStatement preparedStatement = connection.prepareStatement(
-        UPDATE_QUERY
-      )
-    ) {
+    try (Connection connection = DBUtil.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY)) {
       preparedStatement.setString(1, category);
       preparedStatement.setString(2, title);
       preparedStatement.setBigDecimal(3, bdValue);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,14 @@
 module com.ecohub {
   requires javafx.controls;
   requires javafx.fxml;
+  requires javafx.base;
   requires com.jfoenix;
   requires de.jensd.fx.glyphs.commons;
   requires de.jensd.fx.glyphs.fontawesome;
   requires java.sql;
 
   opens com.ecohub to javafx.fxml;
-  exports com.ecohub ;
+  opens com.ecohub.models to java.base, javafx.base;
+
+  exports com.ecohub;
 }
