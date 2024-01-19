ecbf6de3f0ac1460407e2f5b8269cbc038498dfb
Jisi-A <7216lyc@gmail.com>
2024-01-19 19:06:01 +0800

    🗃️get userid from database

--- src/main/java/com/ecohub/DashboardController.java
+++ src/main/java/com/ecohub/DashboardController.java
@@ -1,17 +1,22 @@
 package com.ecohub;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
 
 import com.ecohub.dao.RecordDAO;
+import com.ecohub.models.User;
+import com.ecohub.dailyInputController;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
@@ -26,6 +31,17 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
 
 public class DashboardController {
+
+    private User user;
+
+    private boolean isUserInitialized = false;
+
+    public void initUser(User user) {
+        this.user = user;
+        this.isUserInitialized = true;
+        updateLabel();
+        updateChart();
+    }
     
     @FXML
     private LineChart<String, Number> carbonChart;
@@ -53,8 +69,28 @@ public class DashboardController {
     void updateLabel() {
         RecordDAO recordDAO = new RecordDAO();
         try {
-            BigDecimal total = recordDAO.getTotal(1);
-            carbonData.setText(String.valueOf(total));
+            BigDecimal total = recordDAO.getTotal(user.getUser_id());
+            if (total == null) {
+                carbonData.setText("0");
+            }
+            else {
+                carbonData.setText(String.valueOf(total));
+            }
+            total = recordDAO.getCategory(user.getUser_id(), 2);
+            if (total == null) {
+                electricData.setText("0");
+            }
+            else {
+                electricData.setText(String.valueOf(total));
+            }
+            total = recordDAO.getCategory(user.getUser_id(), 1);
+            if (total == null) {
+                distanceData.setText("0");
+            }
+            else {
+                distanceData.setText(String.valueOf(total));
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -71,7 +107,7 @@ public class DashboardController {
         yAxis.setLabel("Carbon Footprint");
 
         try {
-            String[][] data = recordDAO.getRecent(1); 
+            String[][] data = recordDAO.getRecent(user.getUser_id()); 
 
             XYChart.Series<String, Number> series = new XYChart.Series<String, Number>();
             series.setName("Total Carbon Footprint");
@@ -97,7 +133,7 @@ public class DashboardController {
         List<String[]> data = null;
 
         try {
-            data = recordDAO.getPercentage();
+            data = recordDAO.getPercentage(user.getUser_id());
         } catch (SQLException e) {
             e.printStackTrace();
             // Handle the exception appropriately for your application
@@ -115,7 +151,6 @@ public class DashboardController {
             breakChart.setData(pieChartData);
             breakChart.setClockwise(true);
             breakChart.setLabelLineLength(50);
-            breakChart.setLabelsVisible(true);
             breakChart.setStartAngle(180);
         }
     }
@@ -123,7 +158,9 @@ public class DashboardController {
 
     @FXML
     void initialize() {
-        updateLabel();
-        updateChart();
+        if (isUserInitialized) {
+            updateLabel();
+            updateChart();
+        }
     }
 }
